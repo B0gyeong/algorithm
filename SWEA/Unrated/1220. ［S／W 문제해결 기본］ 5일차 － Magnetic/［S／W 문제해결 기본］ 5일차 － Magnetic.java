94d55f90ab1f56425b276ce071922e8630ed7ed7
최보경 <115474637+B0gyeong@users.noreply.github.com>
2026-02-03 13:30:16 +0900

    [Unrated] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 314  ms, Memory: 100080  KB - DashHub

--- SWEA/Unrated/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
+++ SWEA/Unrated/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
@@ -2,30 +2,29 @@ import java.util.*;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		for(int tc=1; tc<=10; tc++) {
-			int cnt = 0;
-			int N = sc.nextInt();
-			int [][] matrix = new int[N][N];
-			for(int row=0; row<N; row++) {
-				for(int col=0; col<N; col++) {
-					matrix[row][col] = sc.nextInt(); 
-				}
-			}
-			for(int row=0; row<N; row++) {
-				for(int col=0; col<N; col++) {
-					if(matrix[row][col]==1) {
-						for(int x=row+1; x<N; x++) {
-							if(matrix[x][col]==2) {
-								cnt++;
-								break;
-							} else if(matrix[x][col]==1) {
-								break;
-							}
-						}
-					}
-				}
-			}
-			System.out.println("#"+tc+" "+cnt);
-		}
+        for(int tc=1; tc<=10; tc++) {
+            int cnt = 0;
+            int N = sc.nextInt();
+            int [][] matrix = new int[N][N];
+            for(int row=0; row<N; row++) {
+                for(int col=0; col<N; col++) {
+                    matrix[row][col] = sc.nextInt(); 
+                }
+            }
+            for(int col=0; col<N; col++) {
+            	int isN = 0;
+                for(int row=0; row<N; row++) {
+                    if(matrix[row][col]==1) {
+                    	isN = 1;
+                    } else if(matrix[row][col]==2) {
+                    	if(isN == 1) {
+                    		cnt++;
+                    		isN = 0;
+                    	}
+                    }
+                }
+            }
+            System.out.println("#"+tc+" "+cnt);
+        }
 	}
 }
