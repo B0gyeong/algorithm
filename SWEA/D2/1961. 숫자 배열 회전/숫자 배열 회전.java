90f93d60132e73053ac0dffa6440b22fd9638852
최보경 <115474637+B0gyeong@users.noreply.github.com>
2026-02-03 17:08:04 +0900

    [D2] Title: 숫자 배열 회전, Time: 135  ms, Memory: 26752  KB - DashHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -1,9 +1,10 @@
 import java.util.*;
+
 public class Solution {
 	private static void turn90(int[][] matrix, int[][] matrix90 , int N) {
 		for(int row=0; row<N; row++) {
     		for(int col=0; col<N; col++) {
-    			matrix90[row][col] = matrix[-col+N-1][row];
+    			matrix90[row][col] = matrix[N-1-col][row];
     		}
     	}
 		
@@ -11,7 +12,7 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        for(int tc=1; tc<T; tc++) {
+        for(int tc=1; tc<=T; tc++) {
         	int N = sc.nextInt();
         	int [][] matrix = new int [N][N];
         	for(int row=0; row<N; row++) {
@@ -26,21 +27,23 @@ public class Solution {
         	turn90(matrix90,matrix180,N);
         	turn90(matrix180,matrix270,N);
         	
-            System.out.println("#"+tc);
+        	System.out.println("#"+tc);
+            StringBuilder sb = new StringBuilder();
         	for(int row=0; row<N; row++) {
         		for(int col=0; col<N; col++) {
-        			System.out.print(matrix90[row][col]);
+        			sb.append(matrix90[row][col]);
         		}
-        		System.out.print(" ");
+        		sb.append(" ");
         		for(int col=0; col<N; col++) {
-        			System.out.print(matrix180[row][col]);
+        			sb.append(matrix180[row][col]);
         		}
-        		System.out.print(" ");
+        		sb.append(" ");
         		for(int col=0; col<N; col++) {
-        			System.out.print(matrix270[row][col]);
+        			sb.append(matrix270[row][col]);
         		}
-        		System.out.println();
+        		if (row < N - 1) sb.append("\n");
         	}
+            System.out.println(sb.toString());
         }
     }
 
