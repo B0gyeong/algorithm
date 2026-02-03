6655609bbca372dd715583868eab483cfea727a8
최보경 <115474637+B0gyeong@users.noreply.github.com>
2026-02-03 13:41:18 +0900

    [Unrated] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 490  ms, Memory: 103124  KB - DashHub

--- SWEA/Unrated/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/Unrated/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,57 +1,35 @@
 import java.util.*;
 public class Solution {
-	private static int rowMax(int max, int[][] matrix) {
-		int maxFinal = max;
-		for(int row=0; row<100; row++) {
-			int num = 0;
-			for(int col=0; col<100; col++) {
-				num += matrix[row][col];
-			}
-			maxFinal = Math.max(num, maxFinal);
-		}
-		return maxFinal;
-	}
-	private static int colMax(int max, int[][] matrix) {
-		int maxFinal = max;
-		for(int col=0; col<100; col++) {
-			int num = 0;
-			for(int row=0; row<100; row++) {
-				num += matrix[row][col];
-			}
-			maxFinal = Math.max(num, maxFinal);
-		}
-		return maxFinal;
-	}
-	private static int crossMax(int max, int[][] matrix) {
-		int maxFinal = max;
-		int numA = 0;
-		int numB = 0;
-		for(int row=0; row<100; row++) {
-			numA += matrix[row][row];
-		}
-		for(int row=0; row<100; row++) {
-			numB += matrix[99-row][row];
-		}
-		maxFinal = Math.max(Math.max(numA,numB), maxFinal);
-		return maxFinal;
-	}
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		for(int tc=1; tc<=10; tc++) {
-			int testCase = sc.nextInt();
-			int N = 100;
-			int max = 0;
-			int [][] matrix = new int[N][N];
-			for(int row=0; row<N; row++) {
-				for(int col=0; col<N; col++) {
-					matrix[row][col] = sc.nextInt(); 
-				}
-			}
-			max = rowMax(max, matrix);
-			max = colMax(max, matrix);
-			max = crossMax(max, matrix);
-			System.out.println("#"+testCase+" "+max);
-		}
+        for(int tc=1; tc<=10; tc++) {
+            int testCase = sc.nextInt();
+            int N = 100;
+            int max = 0;
+            int [][] matrix = new int[N][N];
+            for(int row=0; row<N; row++) {
+                for(int col=0; col<N; col++) {
+                    matrix[row][col] = sc.nextInt(); 
+                }
+            }
+            int finalMax = 0;
+            int diaSum1 = 0;
+            int diaSum2 = 0;
+            
+            for (int i = 0; i < 100; i++) {
+                int rowSum = 0;
+                int colSum = 0;
+                for (int j = 0; j < 100; j++) {
+                    rowSum += matrix[i][j]; 
+                    colSum += matrix[j][i]; 
+                }
+                finalMax = Math.max(finalMax, Math.max(rowSum, colSum));
+                diaSum1 += matrix[i][i];
+                diaSum2 += matrix[i][99 - i];
+            }
+            finalMax = Math.max(finalMax, Math.max(diaSum1, diaSum2));
+            
+            System.out.println("#"+testCase+" "+finalMax);
+        }
 	}
 }
