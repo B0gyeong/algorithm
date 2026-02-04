fd8d713e972c359ffafbe476f19fd0c87c8cb3bc
최보경 <115474637+B0gyeong@users.noreply.github.com>
2026-02-04 15:23:24 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 길찾기, Time: 135  ms, Memory: 33300  KB - DashHub

--- SWEA/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.java
+++ SWEA/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.java
@@ -2,49 +2,53 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        	for(int tc=0; tc<10; tc++) {
-        		int [] g1 = new int [100];
-        		int [] g2 = new int [100];
-        		Stack<Integer> st = new Stack<>();
-        		boolean [] visited = new boolean [100];
-        		int testcase = sc.nextInt();
-        		int edgeNum = sc.nextInt();
-        		for(int i=0; i<edgeNum; i++) {
-        			int num1 = sc.nextInt();
-        			int num2 = sc.nextInt();
-        			if(g1[num1]==0) {
-        				g1[num1]=num2;
-        			}else {
-        				g2[num1]=num2;
-        			}
-        		}
-        		st.push(g1[0]);
-        		visited[0] = true;
-        		boolean find = false;
-        		while (!st.isEmpty()) {
-					int curNode = st.pop();
-					if(curNode == 99) {
-						find = true;
-						break;
-					}
-					int nextNum1 = g1[curNode];
-					int nextNum2 = g2[curNode];
-					if(!visited[nextNum1]) {
-						st.push(nextNum1);
-						visited[nextNum1] = true;
-					}
-					if(!visited[nextNum2]) {
-						st.push(nextNum2);
-						visited[nextNum2] = true;
-					}
-					
-				}
-        		if(find) {
-        			System.out.println("#"+testcase+" "+1);
-        		} else {
-        			System.out.println("#"+testcase+" "+0);
-        		}
-        	}
+    	Scanner sc = new Scanner(System.in);
+        for(int tc=0; tc<10; tc++) {
+            int [] g1 = new int [100];
+            int [] g2 = new int [100];
+            Arrays.fill(g1, -1);
+            Arrays.fill(g2, -1);
+            
+            Stack<Integer> st = new Stack<>();
+            boolean [] visited = new boolean [100];
+            
+            int testcase = sc.nextInt();
+            int edgeNum = sc.nextInt();
+            
+            for(int i=0; i<edgeNum; i++) {
+                int num1 = sc.nextInt();
+                int num2 = sc.nextInt();
+                
+                if(g1[num1]==-1) g1[num1]=num2;
+                else g2[num1]=num2;
+            }
+            
+            st.push(0);
+            visited[0] = true;
+            
+            boolean find = false;
+            
+            while (!st.isEmpty()) {
+                int curNode = st.pop();
+                if(curNode == 99) {
+                    find = true;
+                    break;
+                }
+                
+                int nextNum1 = g1[curNode];
+                int nextNum2 = g2[curNode];
+                
+                if(nextNum1 != -1 && !visited[nextNum1]) {
+                    st.push(nextNum1);
+                    visited[nextNum1] = true;
+                }
+                if(nextNum2 != -1 && !visited[nextNum2]) {
+                    st.push(nextNum2);
+                    visited[nextNum2] = true;
+                }
+                 
+            }
+            System.out.println("#"+testcase+" "+(find?1:0));
+        }
     }
 }
