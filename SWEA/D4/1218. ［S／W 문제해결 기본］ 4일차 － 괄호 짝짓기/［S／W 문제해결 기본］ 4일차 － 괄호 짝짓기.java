51a21df36f65e76c66a521d569bf3a8bed5ca93e
최보경 <115474637+B0gyeong@users.noreply.github.com>
2026-02-04 15:07:00 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 97  ms, Memory: 27008  KB - DashHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -3,66 +3,37 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         for(int tc=1; tc<=10; tc++) {
-        	int ans = -1;
         	int L = sc.nextInt();
         	String S = sc.next();
         	char ary[] = S.toCharArray();
+        	
         	Stack<Character> st = new Stack<>();
+        	int ans = 1;
+        	
         	for(int i=0; i<L; i++) {
         		char c = ary[i];
+        		
         		if(c=='(' || c=='{' || c=='[' || c=='<') {
         			st.push(c);
-        		} else if(c==')') {
-        			if(st.isEmpty()) {
-        				ans = 0;
-        				break;
-        			}
-        			char closeC = st.pop();
-        			if(closeC == '(') {
-        				ans = 1;
-        			} else {
-        				ans = 0;
-        				break;
-					}
-        		} else if(c=='}') {
-        			if(st.isEmpty()) {
-        				ans = 0;
-        				break;
-        			}
-        			char closeC = st.pop();
-        			if(closeC == '{') {
-        				ans = 1;
-        			} else {
-        				ans = 0;
-        				break;
-					}
-        		} else if(c==']') {
-        			if(st.isEmpty()) {
-        				ans = 0;
-        				break;
-        			}
-        			char closeC = st.pop();
-        			if(closeC == '[') {
-        				ans = 1;
-        			} else {
-        				ans = 0;
-        				break;
-					}
         		} else {
         			if(st.isEmpty()) {
         				ans = 0;
         				break;
         			}
-        			char closeC = st.pop();
-        			if(closeC == '<') {
-        				ans = 1;
-        			} else {
+        			char top = st.pop();
+        			if(!((top == '(' && c == ')') ||
+                            (top == '{' && c == '}') ||
+                            (top == '[' && c == ']') ||
+                            (top == '<' && c == '>'))) {
         				ans = 0;
         				break;
-					}
+        			}
         		}
         	}
+        	if(!st.isEmpty()) ans = 0;
+
         	System.out.println("#"+tc+" "+ans);
         }
     }
