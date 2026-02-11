3174c15d0da2c25cbe403eb3fad5d15130773879
최보경 <115474637+B0gyeong@users.noreply.github.com>
2026-02-11 11:34:25 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 269 ms, Memory: 59,652 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -2,64 +2,66 @@ import java.util.*;
 
 public class Solution {
 	static int N;
-	static int minDiff;
-	static ArrayList<Integer> A;
-	static ArrayList<Integer> B;
-	static int [][] matrix;
+	static int minVal;
 	static boolean [] visited;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for(int tc=1; tc<=T; tc++) {
-			
-			N = sc.nextInt();
-			matrix = new int [N][N];
-			visited = new boolean [N];
-			minDiff = Integer.MAX_VALUE;
-			for(int r=0; r<N; r++) {
-				for(int c=0; c<N; c++) {
-					matrix[r][c] = sc.nextInt();
-				}
-			}
-			
-			comb(0,0);
-			System.out.println("#"+tc+" "+minDiff);
+	static int [][] matrix;
+    public static void main(String[] args){
+    	Scanner sc = new Scanner(System.in);
+    	int T = sc.nextInt();
+    	for(int tc=1; tc<=T; tc++) {
+    		N = sc.nextInt();
+    		matrix = new int [N][N];
+    		for(int r=0; r<N; r++) {
+    			for(int c=0; c<N; c++) {
+    				matrix[r][c] = sc.nextInt();
+    			}
+    		}
+    		
+    		
+    		visited = new boolean [N];
+    		minVal = Integer.MAX_VALUE;
+    		solve(0, 0);
+    		System.out.println("#"+tc+" "+minVal);
+    	}
+	    
+	}
+
+	private static void solve(int k, int s) {
+		// N개 중에 N/2개를 뽑는 조합
+		// 뽑히지 않는 조합도 확인해야되기 때문에 boolean 배열 이용
+		if(k == N/2) {
+			minVal = Math.min(minVal, calculate());
+			return;
+		}
+		
+		for(int i=s; i<N; i++) {
+			visited[i] = true;
+			solve(k+1, i+1);
+			visited[i] = false;
 		}
 	}
-	
-	private static void comb(int k, int s) {
-		if(k == N/2) {
-			A = new ArrayList<>();
-			B = new ArrayList<>();
-			for(int i=0; i<N; i++) {
-				if(visited[i]) {
-					A.add(i);
-				} else {
-					B.add(i);
-				}
-			}
-			calculDiff();
-		} else {
-			for(int i=s; i<N; i++) {
-				visited[i] = true;
-				comb(k+1, i+1);
-				visited[i] = false;
+
+	private static int calculate() {
+		// 두개의 배열을 생성하고 각각의 시너지 합을 계산
+		// 각 합의 차를 return
+		List<Integer> foodA = new ArrayList<>();
+		List<Integer> foodB = new ArrayList<>();
+		for(int i=0; i<N; i++) {
+			if(visited[i]) foodA.add(i);
+			else foodB.add(i);
+		}
+		
+		int sumA = 0; int sumB = 0;
+		for(int i=0; i<N/2-1; i++) {
+			for(int j=i+1; j<N/2; j++) {
+				sumA+=matrix[foodA.get(i)][foodA.get(j)];
+				sumA+=matrix[foodA.get(j)][foodA.get(i)];
+				sumB+=matrix[foodB.get(i)][foodB.get(j)];
+				sumB+=matrix[foodB.get(j)][foodB.get(i)];
 			}
 		}
 		
-	}
-
-	private static void calculDiff() {
-		int aSum = 0; int bSum = 0;
-		for(int i=0; i<N/2-1; i++) {
-			for(int j=i+1; j<N/2; j++) {
-				aSum += matrix[A.get(i)][A.get(j)];
-				aSum += matrix[A.get(j)][A.get(i)];
-				bSum += matrix[B.get(i)][B.get(j)];
-				bSum += matrix[B.get(j)][B.get(i)];
-			}
-		}
-		if(Math.abs(aSum-bSum) < minDiff) minDiff = Math.abs(aSum-bSum);
+		return Math.abs(sumA - sumB);
 	}
 }
+
