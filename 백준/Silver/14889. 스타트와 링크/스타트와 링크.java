fc6e75b8d63315748332c1b1dcf95392ba95f11b
최보경 <115474637+B0gyeong@users.noreply.github.com>
2026-02-18 01:06:37 +0900

    [Silver I] Title: 스타트와 링크, Time: 376 ms, Memory: 35328 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,60 +1,63 @@
 import java.util.*;
 
 public class Main {
-	static int N;
+	static int N, minDiff;
 	static boolean [] visited;
 	static int [][] matrix;
-	static int minAns;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		matrix = new int [N][N];
-		
-		visited = new boolean [N];
-		
-		for(int r=0; r<N; r++) {
-			for(int c=0; c<N; c++) {
-				matrix[r][c] = sc.nextInt();
-			}
-		}
-		minAns = 100000;
-		comb(0, 0);
-		
-		System.out.println(minAns);
+    public static void main(String[] args){
+    	Scanner sc = new Scanner(System.in);
+    	N = sc.nextInt();
+    	matrix = new int [N][N];
+    	for(int r=0; r<N; r++) {
+    		for(int c=0; c<N; c++) {
+    			matrix[r][c] = sc.nextInt();
+    		}
+    	}
+    	
+    	minDiff = Integer.MAX_VALUE;
+    	visited = new boolean [N];
+    	visited[0] = true;
+    	solve(1, 1);
+    	System.out.println(minDiff);
 	}
 
-	private static void comb(int k, int s) {
-		if(k == N/2) {
-			ArrayList<Integer> start = new ArrayList<>();
-			ArrayList<Integer> link = new ArrayList<>();
-			int startSum = 0;
-			int linkSum = 0;
-			for(int i=0; i<N; i++) {
-				if(visited[i]) {
-					start.add(i);
-				} else {
-					link.add(i);
-				}
-			}
-			
-			for(int i=0; i<N/2; i++) {
-				for(int j=0; j<N/2; j++) {
-					if(i != j) {
-						startSum+=matrix[start.get(i)][start.get(j)];
-						linkSum+=matrix[link.get(i)][link.get(j)];
-					}
-				}
-			}
-			
-			int abSum = Math.abs(startSum-linkSum);
-			minAns = Math.min(minAns, abSum);
-		} else {
-			for(int i=s; i< N; i++) {
-				visited[i] = true;
-				comb(k+1, i+1);
-				visited[i] = false;
-			}
+	private static void solve(int idx, int s) {
+		if(idx==N/2) {
+			calculate();
+			return;
 		}
 		
+		for(int i=s; i<N; i++) {
+			visited[i] = true;
+			solve(idx+1, i+1);
+			visited[i] = false;
+		}
+		
+	}
+
+	private static void calculate() {
+		List <Integer> start = new ArrayList<>();
+		List <Integer> link = new ArrayList<>();
+		for(int i=0; i<N; i++) {
+			if(visited[i]) {
+				start.add(i);
+			}
+			else {
+				link.add(i);
+			}
+			
+		}
+
+		int sSum = 0;
+		int lSum = 0;
+		for(int i=0; i<N/2-1; i++) {
+			for(int j=i; j<N/2; j++) {
+				sSum += matrix[start.get(i)][start.get(j)];
+				sSum += matrix[start.get(j)][start.get(i)];
+				lSum += matrix[link.get(i)][link.get(j)];
+				lSum += matrix[link.get(j)][link.get(i)];
+			}
+		}
+		minDiff = Math.min(minDiff, Math.abs(sSum-lSum));
 	}
 }
