b82ec3f34cf1611a47dc2e693899fbcf02aea091
최보경 <115474637+B0gyeong@users.noreply.github.com>
2026-03-02 14:31:28 +0900

    [D4] Title: 동철이의 일 분배, Time: 2,093 ms, Memory: 38,812 KB -BaekjoonHub

--- SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.java
+++ SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.java
@@ -1,52 +1,47 @@
 import java.util.*;
 
-public class Solution {
-    static int N;
-    static double maxNum;
-    static int [][] P;
-    static boolean [] visited;
+public class Solution {	
+	static int N;
+	static double maxSum;
+	static int [][] matrix;
+	static boolean [] visited;
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
         
         int T = sc.nextInt();
+        
         for(int tc=1; tc<=T; tc++) {
-        	
         	N = sc.nextInt();
-        	
-        	P = new int [N][N];
-        	visited = new boolean [N];
-        	
-        	
-        	for(int r=0; r<N; r++) {
+        	matrix = new int [N][N];
+        	for(int r=0; r<N; r++ ) {
         		for(int c=0; c<N; c++) {
-        			P[r][c] = sc.nextInt();
+        			matrix[r][c] = sc.nextInt();
         		}
         	}
         	
-        	maxNum = 0;
-        	
+        	maxSum = Double.MIN_VALUE;
+        	visited = new boolean [N];
         	perm(0, 1.0);
-        	
-        	System.out.printf("#%d %.6f\n",tc, maxNum*100);
+        	System.out.printf("#%d %.6f",tc, maxSum*100);
+            System.out.println();
         }
-    }
+	}
     
-	private static void perm(int k, double prob) {
-		if (prob <= maxNum) {
-	        return;
-	    }
+    
+	private static void perm(int idx, double sum) {
+		if(sum < maxSum) return;
 		
-		if(k == N) {
-			maxNum = prob;
+		if(idx == N) {
+			maxSum = Math.max(maxSum, sum);
 			return;
 		}
 		
 		for(int i=0; i<N; i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				perm(k+1, prob * (P[k][i] / 100.0));
-				visited[i] = false;
-			} 
+			if(visited[i]) continue;
+			
+			visited[i] = true;
+			perm(idx+1, sum * ((double) matrix[idx][i] / 100));
+			visited[i] = false;
 		}
-	} 
+	}
 }
