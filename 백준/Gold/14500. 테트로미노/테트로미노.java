ed22120da8ec05340990b17dbf1826f49fc22e5a
최보경 <115474637+B0gyeong@users.noreply.github.com>
2026-03-13 14:12:39 +0900

    [Gold IV] Title: 테트로미노, Time: 668 ms, Memory: 37932 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -2,8 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, M;
+	static int N, M, sum;
+	static int [] dr = {-1, 1, 0, 0};
+	static int [] dc = {0, 0, -1, 1};
 	static int [][] map;
+	static boolean [][] visited;
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,56 +22,53 @@ public class Main {
 			}
 		}
 		
-		int sum = 0;
+		visited = new boolean [N][M];
+		sum = 0;
 		for(int r=0; r<N; r++) {
-			for(int c=0; c<=M-4; c++) {
-				sum = Math.max(sum, map[r][c] + map[r][c+1] + map[r][c+2] + map[r][c+3]);
-			}
-		}
-		
-		for(int r=0; r<=N-4; r++) {
 			for(int c=0; c<M; c++) {
-				sum = Math.max(sum, map[r][c] + map[r+1][c] + map[r+2][c] + map[r+3][c]);
-			}
-		}
-		
-		for(int r=0; r<=N-2; r++) {
-			for(int c=0; c<=M-2; c++) {
-				sum = Math.max(sum, map[r][c] + map[r+1][c+1] + map[r+1][c] + map[r][c+1]);
-			}
-		}
-		
-		for(int r=0; r<=N-3; r++) {
-			for(int c=0; c<=M-2; c++) {
-				sum = Math.max(sum, map[r][c] + map[r][c+1] + map[r+1][c+1] + map[r+2][c+1]);
-				sum = Math.max(sum, map[r][c] + map[r][c+1] + map[r+1][c] + map[r+2][c]);
-				sum = Math.max(sum, map[r][c] + map[r+1][c] + map[r+2][c] + map[r+2][c+1]);
-				sum = Math.max(sum, map[r][c+1] + map[r+1][c+1] + map[r+2][c+1] + map[r+2][c]);
-				
-				sum = Math.max(sum, map[r][c] + map[r+1][c] + map[r+1][c+1] + map[r+2][c+1]);
-				sum = Math.max(sum, map[r][c+1] + map[r+1][c] + map[r+1][c+1] + map[r+2][c]);
-				
-				sum = Math.max(sum, map[r][c] + map[r+1][c] + map[r+2][c] + map[r+1][c+1]);
-				sum = Math.max(sum, map[r][c+1] + map[r+1][c+1] + map[r+2][c+1] + map[r+1][c]);
-			}
-		}
-		
-		for(int r=0; r<=N-2; r++) {
-			for(int c=0; c<=M-3; c++) {
-				sum = Math.max(sum, map[r+1][c] + map[r+1][c+1] + map[r+1][c+2] + map[r][c+2]);
-				sum = Math.max(sum, map[r][c] + map[r+1][c] + map[r+1][c+1] + map[r+1][c+2]);
-				sum = Math.max(sum, map[r][c] + map[r][c+1] + map[r][c+2] + map[r+1][c]);
-				sum = Math.max(sum, map[r][c] + map[r][c+1] + map[r][c+2] + map[r+1][c+2]);
-				
-				sum = Math.max(sum, map[r][c+1] + map[r][c+2] + map[r+1][c] + map[r+1][c+1]);
-				sum = Math.max(sum, map[r][c] + map[r][c+1] + map[r+1][c+1] + map[r+1][c+2]);
-				
-				sum = Math.max(sum, map[r][c+1] + map[r+1][c] + map[r+1][c+1] + map[r+1][c+2]);
-				sum = Math.max(sum, map[r][c] + map[r][c+1] + map[r][c+2] + map[r+1][c+1]);
+				visited[r][c] = true;
+				dfs(r, c, 0, map[r][c]);
+				visited[r][c] = false;
+
+				checkT(r, c);
 			}
 		}
 		
 		System.out.println(sum);
+	}
+	
+	private static void checkT(int r, int c) {
+		// TODO Auto-generated method stub
+		
+		if(r<=N-3 && c<=M-2) {
+			sum = Math.max(sum, map[r][c] + map[r+1][c] + map[r+2][c] + map[r+1][c+1]);
+			sum = Math.max(sum, map[r][c+1] + map[r+1][c+1] + map[r+2][c+1] + map[r+1][c]);
+		}
+		
+		if(r<=N-2 && c<=M-3) {
+			sum = Math.max(sum, map[r][c+1] + map[r+1][c] + map[r+1][c+1] + map[r+1][c+2]);
+			sum = Math.max(sum, map[r][c] + map[r][c+1] + map[r][c+2] + map[r+1][c+1]);
+		}
+	}
+
+	private static void dfs(int r, int c, int k, int sumF) {
+		if(k == 3) {
+			if (sumF > sum) sum = sumF;
+			return;
+		}
+		
+		for(int d=0; d<4; d++) {
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			
+			if(nr<0||nr>=N||nc<0||nc>=M||visited[nr][nc]) continue;
+			
+			visited[nr][nc] = true;
+			dfs(nr, nc, k+1, sumF+map[nr][nc]);
+			visited[nr][nc] = false;
+			
+		}
+		
 	}
 
 }
