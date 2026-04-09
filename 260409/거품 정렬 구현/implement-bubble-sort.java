90e16f68df163227eb228de06ba307f5f106ea2e
최보경 <115474637+B0gyeong@users.noreply.github.com>
2026-04-09 16:59:38 +0900

    Title: 거품 정렬 구현, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260409/거품 정렬 구현/implement-bubble-sort.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int [] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num;
+        }
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N-1-i; j++){
+                if(arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+
+        for(int i=0; i<N; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
